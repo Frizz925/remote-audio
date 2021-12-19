3fdc1007a711e44d3bdbdc57ee00180f07d7c7bd
Izra Faturrahman <Frizz925@hotmail.com>
2021-12-20 05:27:01 +0700

    Rewrite again

--- src/types.h
+++
@@ -1,8 +0,0 @@
-#ifndef _H_TYPES
-#define _H_TYPES
-
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long uint64;
-#endif
--- src/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _H_UTILS
-#define _H_UTILS
-
-#include <arpa/inet.h>
-
-int straddr_p(struct sockaddr_in *saddr, char *buf);
-const char *straddr(struct sockaddr_in *saddr);
-#endif
