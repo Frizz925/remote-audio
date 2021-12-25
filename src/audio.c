0c9163ad1202b51cbad83e8afb6ebdd88604619a
Izra Faturrahman <Frizz925@hotmail.com>
2021-12-26 06:02:13 +0700

    audio: Implement find device and create stream

--- src/audio.c
+++ src/audio.c
@@ -1,6 +1,11 @@
 #include "audio.h"
 
 #include <stdio.h>
+#include <string.h>
+
+static void print_pa_error(const char *cause, int err) {
+    fprintf(stderr, "%s: (%d) %s\n", cause, err, Pa_GetErrorText(err));
+}
 
 int ra_audio_init() {
     int err = Pa_Initialize();
@@ -13,4 +18,58 @@ int ra_audio_init() {
 
 void ra_audio_deinit() {
     Pa_Terminate();
+}
+
+PaStream *ra_audio_create_stream(const char *dev, ra_find_device_type type, PaStreamCallback *callback,
+                                 void *userdata) {
+    PaStreamParameters params = {0};
+    params.device = ra_audio_find_device(dev, type);
+    if (params.device == paNoDevice) {
+        return NULL;
+    }
+    const PaDeviceInfo *info = Pa_GetDeviceInfo(params.device);
+    if (type == RA_AUDIO_DEVICE_INPUT)
+        printf("Using device input for source: %s\n", info->name);
+    else
+        printf("Using device output for sink: %s\n", info->name);
+    params.channelCount = CHANNELS;
+    params.sampleFormat = PA_SAMPLE_TYPE;
+    params.suggestedLatency =
+        type == RA_AUDIO_DEVICE_INPUT ? info->defaultLowInputLatency : info->defaultLowOutputLatency;
+
+    PaStream *stream;
+    PaStreamParameters *inparams = type == RA_AUDIO_DEVICE_INPUT ? &params : NULL;
+    PaStreamParameters *outparams = type == RA_AUDIO_DEVICE_OUTPUT ? &params : NULL;
+    int err = Pa_OpenStream(&stream, inparams, outparams, SAMPLE_RATE, FRAMES_PER_BUFFER, 0, callback, userdata);
+    if (err != paNoError) {
+        print_pa_error("Failed to open stream", err);
+        return NULL;
+    }
+    return stream;
+}
+
+PaDeviceIndex ra_audio_find_device(const char *dev, ra_find_device_type type) {
+    const char *devtype = type == RA_AUDIO_DEVICE_INPUT ? "input" : "output";
+    if (!dev) {
+        PaDeviceIndex index = type == RA_AUDIO_DEVICE_INPUT ? Pa_GetDefaultInputDevice() : Pa_GetDefaultOutputDevice();
+        if (index != paNoDevice) return index;
+        fprintf(stderr, "No default %s device found\n", devtype);
+        return paNoDevice;
+    }
+
+    PaDeviceIndex index;
+    const PaDeviceInfo *info;
+    int count = Pa_GetDeviceCount();
+    for (index = 0; index < count; index++) {
+        info = Pa_GetDeviceInfo(index);
+        if (strncasecmp(dev, info->name, strlen(dev)))
+            continue;
+        else if (type == RA_AUDIO_DEVICE_INPUT && info->maxInputChannels < CHANNELS)
+            continue;
+        else if (type == RA_AUDIO_DEVICE_OUTPUT && info->maxOutputChannels < CHANNELS)
+            continue;
+        return index;
+    }
+    fprintf(stderr, "No %s device found: %s\n", devtype, dev);
+    return paNoDevice;
 }
--- src/audio.h
+++ src/audio.h
@@ -1,20 +1,32 @@
 #ifndef _RA_AUDIO_H
 #define _RA_AUDIO_H
 
-#include <portaudio.h>
 #include <opus/opus.h>
+#include <portaudio.h>
 
 #include "stream.h"
 
 #define SAMPLE_RATE 48000
+#define SAMPLE_SIZE 4
 #define FRAMES_PER_BUFFER 960
 #define CHANNELS 2
 #define APPLICATION OPUS_APPLICATION_RESTRICTED_LOWDELAY
 
+#define DECODE_BUFFER_SIZE CHANNELS * 5760
+#define ENCODE_BUFFER_SIZE DECODE_BUFFER_SIZE
+#define RING_BUFFER_SIZE 2 * SAMPLE_SIZE *DECODE_BUFFER_SIZE
+
 #define PA_SAMPLE_TYPE paFloat32;
 typedef float SAMPLE;
+typedef enum {
+    RA_AUDIO_DEVICE_OUTPUT,
+    RA_AUDIO_DEVICE_INPUT,
+} ra_find_device_type;
 
 int ra_audio_init();
 void ra_audio_deinit();
+
+PaStream *ra_audio_create_stream(const char *dev, ra_find_device_type type, PaStreamCallback *callback, void *userdata);
+PaDeviceIndex ra_audio_find_device(const char *dev, ra_find_device_type type);
 
 #endif
