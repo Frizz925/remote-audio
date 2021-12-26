3fec8413937dede5f094a6740682d7370119764b
Izra Faturrahman <Frizz925@hotmail.com>
2021-12-26 09:06:51 +0700

    types: Remove uint64 raw type

--- src/types.c
+++ src/types.c
@@ -1,7 +1,7 @@
 #include "types.h"
 
-#include <arpa/inet.h>
-#include <string.h>
+#include "socket.h"
+#include "string.h"
 
 void uint16_to_bytes(char *buf, uint16_t value) {
     uint16_raw_t raw = {.value = htons(value)};
@@ -10,11 +10,6 @@ void uint16_to_bytes(char *buf, uint16_t value) {
 
 void uint32_to_bytes(char *buf, uint32_t value) {
     uint32_raw_t raw = {.value = htonl(value)};
-    memcpy(buf, raw.buf, sizeof(raw));
-}
-
-void uint64_to_bytes(char *buf, uint64_t value) {
-    uint64_raw_t raw = {.value = htonll(value)};
     memcpy(buf, raw.buf, sizeof(raw));
 }
 
@@ -28,10 +23,4 @@ uint32_t bytes_to_uint32(const char *buf) {
     uint32_raw_t raw;
     memcpy(raw.buf, buf, sizeof(raw));
     return ntohl(raw.value);
-}
-
-uint64_t bytes_to_uint64(const char *buf) {
-    uint64_raw_t raw;
-    memcpy(raw.buf, buf, sizeof(raw));
-    return ntohll(raw.value);
 }
--- src/types.h
+++ src/types.h
@@ -12,16 +12,9 @@ typedef union {
     char buf[4];
 } uint32_raw_t;
 
-typedef union {
-    uint64_t value;
-    char buf[8];
-} uint64_raw_t;
-
 void uint16_to_bytes(char *buf, uint16_t value);
 void uint32_to_bytes(char *buf, uint32_t value);
-void uint64_to_bytes(char *buf, uint64_t value);
 
 uint16_t bytes_to_uint16(const char *buf);
 uint32_t bytes_to_uint32(const char *buf);
-uint64_t bytes_to_uint64(const char *buf);
 #endif
