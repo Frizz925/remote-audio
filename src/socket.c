62945bf3382b7cef093f29eaa7913bf7dcf330b2
Izra Faturrahman <Frizz925@hotmail.com>
2022-01-02 13:52:27 +0700

    socket: Uninterrupted select on unix

--- src/socket.c
+++ src/socket.c
@@ -45,12 +45,29 @@ void ra_gai_perror(const char *msg, int err) {
 }
 #else
 #include <netdb.h>
+#include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "string.h"
 
+static sigset_t *sigmask = NULL;
+
 int ra_socket_init() {
+    sigmask = (sigset_t *)malloc(sizeof(sigset_t));
+    sigemptyset(sigmask);
+    sigaddset(sigmask, SIGINT);
+    sigaddset(sigmask, SIGTERM);
+
     return 0;
+}
+
+int ra_socket_select(int nfds, fd_set *fds, const struct timeval *timeout) {
+    struct timespec ts_timeout = {
+        .tv_sec = timeout->tv_sec,
+        .tv_nsec = timeout->tv_usec * 1000,
+    };
+    return pselect(nfds + 1, fds, NULL, NULL, &ts_timeout, sigmask);
 }
 
 void ra_socket_perror(const char *msg) {
@@ -61,7 +78,9 @@ void ra_socket_close(SOCKET sock) {
     close(sock);
 }
 
-void ra_socket_deinit() {}
+void ra_socket_deinit() {
+    if (sigmask) free(sigmask);
+}
 
 void ra_gai_perror(const char *msg, int err) {
     fprintf(stderr, "%s: %s\n", msg, gai_strerror(err));
