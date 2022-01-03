7efff2d49492834f105733b431bb66591adcd308
Izra Faturrahman <Frizz925@hotmail.com>
2022-01-03 22:53:18 +0700

    thread: Implement thread join timeout

--- src/thread.c
+++ src/thread.c
@@ -8,16 +8,34 @@ typedef struct {
     void *data;
 } thread_context_t;
 
+static thread_context_t *create_thread_context(ra_thread_func *routine, void *data) {
+    thread_context_t *ctx = (thread_context_t *)malloc(sizeof(thread_context_t));
+    ctx->thread_func = routine;
+    ctx->data = data;
+    return ctx;
+}
+
+static void run_thread_context(thread_context_t *ctx) {
+    ra_thread_func *fn = ctx->thread_func;
+    void *data = ctx->data;
+    free(ctx);
+    (*fn)(data);
+}
+
 #ifdef _WIN32
 #include <synchapi.h>
 #include <windows.h>
+
+static void thread_bootstrap(void *arg) {
+    run_thread_context((thread_context_t *)arg);
+}
 
 void ra_sleep(unsigned int seconds) {
     Sleep(seconds * 1000);
 }
 
 ra_thread_t ra_thread_start(ra_thread_func *routine, void *data, int *err) {
-    ra_thread_t handle = _beginthread((_beginthread_proc_type)routine, 0, data);
+    ra_thread_t handle = _beginthread(thread_bootstrap, 0, create_thread_context(routine, data));
     *err = handle == -1L ? errno : 0;
     return handle;
 }
@@ -27,17 +45,17 @@ void ra_thread_exit() {
 }
 
 int ra_thread_join(ra_thread_t thread) {
-    return WaitForSingleObject((HANDLE)thread, INFINITE);
+    return WaitForSingleObject((HANDLE)thread, INFINITE) == WAIT_FAILED;
+}
+
+int ra_thread_join_timeout(ra_thread_t thread, time_t seconds) {
+    return WaitForSingleObject((HANDLE)thread, seconds * 1000);
 }
 #else
 #include <unistd.h>
 
 static void *thread_bootstrap(void *arg) {
-    thread_context_t *ctx = (thread_context_t *)arg;
-    ra_thread_func *fn = ctx->thread_func;
-    void *data = ctx->data;
-    free(ctx);
-    (*fn)(data);
+    run_thread_context((thread_context_t *)arg);
     return NULL;
 }
 
@@ -46,12 +64,8 @@ void ra_sleep(unsigned int seconds) {
 }
 
 ra_thread_t ra_thread_start(ra_thread_func *routine, void *data, int *err) {
-    thread_context_t *ctx = (thread_context_t *)malloc(sizeof(thread_context_t));
-    ctx->thread_func = routine;
-    ctx->data = data;
-
     ra_thread_t thread = 0;
-    *err = pthread_create((pthread_t *)&thread, NULL, &thread_bootstrap, ctx);
+    *err = pthread_create((pthread_t *)&thread, NULL, &thread_bootstrap, create_thread_context(routine, data));
     return thread;
 }
 
--- src/thread.h
+++ src/thread.h
@@ -4,6 +4,8 @@
 #ifdef _WIN32
 #include <process.h>
 
+#define RA_THREAD_WAIT_TIMEOUT WAIT_TIMEOUT
+
 typedef uintptr_t ra_thread_t;
 #else
 #include <pthread.h>
@@ -18,5 +20,6 @@ void ra_sleep(unsigned int seconds);
 ra_thread_t ra_thread_start(ra_thread_func *routine, void *data, int *err);
 void ra_thread_exit();
 int ra_thread_join(ra_thread_t thread);
+int ra_thread_join_timeout(ra_thread_t thread, time_t seconds);
 
 #endif
