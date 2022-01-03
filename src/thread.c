3d6630e4d34c1e7cff8d0db54b1f8fc6b5f4b84b
Izra Faturrahman <Frizz925@hotmail.com>
2022-01-04 00:30:50 +0700

    thread: Implement ra_thread_destroy
    - Remove ra_thread_exit
    - Implement ra_thread_join_timeout for *nix

--- src/thread.c
+++ src/thread.c
@@ -8,23 +8,29 @@ typedef struct {
     void *data;
 } thread_context_t;
 
-static thread_context_t *create_thread_context(ra_thread_func *routine, void *data) {
-    thread_context_t *ctx = (thread_context_t *)malloc(sizeof(thread_context_t));
+static void init_thread_context(thread_context_t *ctx, ra_thread_func *routine, void *data) {
     ctx->thread_func = routine;
     ctx->data = data;
+}
+
+static thread_context_t *create_thread_context(ra_thread_func *routine, void *data) {
+    thread_context_t *ctx = (thread_context_t *)malloc(sizeof(thread_context_t));
+    init_thread_context(ctx, routine, data);
     return ctx;
 }
 
 static void run_thread_context(thread_context_t *ctx) {
-    ra_thread_func *fn = ctx->thread_func;
-    void *data = ctx->data;
-    free(ctx);
-    (*fn)(data);
+    (*ctx->thread_func)(ctx->data);
 }
 
 #ifdef _WIN32
 #include <synchapi.h>
 #include <windows.h>
+
+typedef struct {
+    ra_thread_func *thread_func;
+    void *data;
+} thread_context_t;
 
 static void thread_bootstrap(void *arg) {
     run_thread_context((thread_context_t *)arg);
@@ -51,29 +57,97 @@ int ra_thread_join(ra_thread_t thread) {
 int ra_thread_join_timeout(ra_thread_t thread, time_t seconds) {
     return WaitForSingleObject((HANDLE)thread, seconds * 1000);
 }
+
+int ra_thread_destroy(ra_thread_t ra_thread_t) {
+    return 0;
+}
 #else
+#include <time.h>
 #include <unistd.h>
 
+struct ra_thread_handle_t {
+    pthread_t thread;
+    pthread_mutex_t mutex;
+    pthread_mutexattr_t mutex_attr;
+};
+
+typedef struct {
+    thread_context_t base;
+    pthread_mutex_t *mutex;
+} thread_context_mutex_t;
+
+static void thread_handle_cleanup(ra_thread_t handle) {}
+
 static void *thread_bootstrap(void *arg) {
-    run_thread_context((thread_context_t *)arg);
+    thread_context_mutex_t *ctx = (thread_context_mutex_t *)arg;
+    pthread_mutex_lock(ctx->mutex);
+    run_thread_context((thread_context_t *)ctx);
+    pthread_mutex_unlock(ctx->mutex);
+    free(ctx);
+
+    pthread_exit(NULL);
     return NULL;
 }
+
+#ifdef __APPLE__
+static int pthread_mutex_timedlock(pthread_mutex_t *mutex, const struct timespec *abs_timeout) {
+    while (time(NULL) < abs_timeout->tv_sec) {
+        if (pthread_mutex_trylock(mutex))
+            sleep(1);
+        else
+            return 0;
+    }
+    return ETIMEDOUT;
+}
+#endif
 
 void ra_sleep(unsigned int seconds) {
     sleep(seconds);
 }
 
 ra_thread_t ra_thread_start(ra_thread_func *routine, void *data, int *err) {
-    ra_thread_t thread = 0;
-    *err = pthread_create((pthread_t *)&thread, NULL, &thread_bootstrap, create_thread_context(routine, data));
-    return thread;
+    ra_thread_t handle = (ra_thread_t)malloc(sizeof(ra_thread_t));
+    thread_context_mutex_t *ctx = (thread_context_mutex_t *)malloc(sizeof(thread_context_mutex_t));
+    ctx->mutex = &handle->mutex;
+
+    *err = pthread_mutexattr_init(&handle->mutex_attr);
+    if (*err) goto error;
+    *err = pthread_mutexattr_settype(&handle->mutex_attr, PTHREAD_MUTEX_ERRORCHECK);
+    if (*err) goto error;
+    *err = pthread_mutex_init(&handle->mutex, &handle->mutex_attr);
+    if (*err) goto error;
+
+    init_thread_context((thread_context_t *)ctx, routine, data);
+    *err = pthread_create(&handle->thread, NULL, &thread_bootstrap, ctx);
+    if (*err) goto error;
+
+    return handle;
+
+error:
+    free(ctx);
+    ra_thread_destroy(handle);
+    return 0;
 }
 
-void ra_thread_exit() {
-    pthread_exit(NULL);
+int ra_thread_join(ra_thread_t handle) {
+    return pthread_join(handle->thread, NULL);
 }
 
-int ra_thread_join(ra_thread_t thread) {
-    return pthread_join(thread, NULL);
+int ra_thread_join_timeout(ra_thread_t handle, time_t seconds) {
+    struct timespec abs_timeout = {
+        .tv_sec = time(NULL) + seconds,
+        .tv_nsec = 0,
+    };
+    int res = pthread_mutex_timedlock(&handle->mutex, &abs_timeout);
+    if (res) return res;
+    return pthread_mutex_unlock(&handle->mutex);
+}
+
+int ra_thread_destroy(ra_thread_t handle) {
+    pthread_mutex_destroy(&handle->mutex);
+    pthread_mutexattr_destroy(&handle->mutex_attr);
+    pthread_detach(handle->thread);
+    free(handle);
+    return 0;
 }
 #endif
--- src/thread.h
+++ src/thread.h
@@ -9,8 +9,12 @@
 typedef uintptr_t ra_thread_t;
 #else
 #include <pthread.h>
+#include <errno.h>
 
-typedef pthread_t ra_thread_t;
+#define RA_THREAD_WAIT_TIMEOUT ETIMEDOUT
+
+struct ra_thread_handle_t;
+typedef struct ra_thread_handle_t *ra_thread_t;
 #endif
 
 typedef void ra_thread_func(void *);
@@ -18,8 +22,8 @@ typedef void ra_thread_func(void *);
 void ra_sleep(unsigned int seconds);
 
 ra_thread_t ra_thread_start(ra_thread_func *routine, void *data, int *err);
-void ra_thread_exit();
 int ra_thread_join(ra_thread_t thread);
 int ra_thread_join_timeout(ra_thread_t thread, time_t seconds);
+int ra_thread_destroy(ra_thread_t thread);
 
 #endif
