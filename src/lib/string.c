19e717a88c678ba1f46f79057900c9232cec15d1
Izra Faturrahman <Frizz925@hotmail.com>
2022-01-04 21:48:01 +0700

    lib(string): Move strequal to source

---
+++ src/lib/string.c
@@ -0,0 +1,5 @@
+#include "string.h"
+
+int strequal(const char *c1, const char *c2) {
+    return strcmp(c1, c2) == 0;
+}
--- src/lib/string.h
+++ src/lib/string.h
@@ -8,8 +8,6 @@
 #define strcasecmp _stricmp
 #endif
 
-inline int strequal(const char *c1, const char *c2) {
-    return strcmp(c1, c2) == 0;
-}
+int strequal(const char *c1, const char *c2);
 
 #endif
